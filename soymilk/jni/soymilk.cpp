928e1e3142ebe51688e234a134814306ead9e8e4
YC Tien <yaocheng2010@gmail.com>
2013-05-04 02:08:43 -0700

    Testing again.

--- soymilk/jni/soymilk.cpp
+++ soymilk/jni/soymilk.cpp
@@ -7,7 +7,6 @@ extern "C"
 JNIEXPORT void JNICALL Java_com_ytstudio_soymilk_SplashScreen_NativeInitialize(JNIEnv *env, jobject obj)
 {
    LOGI("SplashScreen", "Testing");
-   LOGI("SplashScreen", "Testing twice");
 }
 
 }
