3dab51e0d4919576179f6d0d431c6a68bf85d678
Vickey Yeh <tzuhanyeh@gmail.com>
2013-05-03 23:32:26 -0700

    Uncommit changes.

---
+++ soymilk/jni/soymilk.cpp
@@ -0,0 +1,12 @@
+#include <jni.h>
+#include <Logger.h>
+
+extern "C"
+{
+
+JNIEXPORT void JNICALL Java_com_ytstudio_soymilk_SplashScreen_NativeInitialize(JNIEnv *env, jobject obj)
+{
+   LOGI("SplashScreen", "Testing");
+}
+
+}
