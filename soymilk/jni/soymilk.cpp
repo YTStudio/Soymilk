5070ed8d92b33b1976ce8f1a62293a9ad79ee995
YC Tien <yaocheng2010@gmail.com>
2013-05-04 02:05:12 -0700

    Test commit.

--- soymilk/jni/soymilk.cpp
+++ soymilk/jni/soymilk.cpp
@@ -7,6 +7,7 @@ extern "C"
 JNIEXPORT void JNICALL Java_com_ytstudio_soymilk_SplashScreen_NativeInitialize(JNIEnv *env, jobject obj)
 {
    LOGI("SplashScreen", "Testing");
+   LOGI("SplashScreen", "Testing twice");
 }
 
 }
